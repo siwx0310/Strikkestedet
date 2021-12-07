8fc40ff9a03a44d12934088a153ea75d17e7e564
Siw Pedersen <siw@charlietango.dk>
2021-12-07 09:58:24 +0100

    feat: second commmit

--- src/Globals/Footer.js
+++ src/Globals/Footer.js
@@ -2,6 +2,8 @@
 
 export default function Footer(){
     return (
+
+
         <div className="bg-gray-footer">
         <div className="max-w-screen-2xl mx-auto grid grid-cols-8 gap-4">
             <h1>This is the footer</h1>
