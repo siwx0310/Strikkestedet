085ae04c6e50cb930e71c231d43936a7c577e7b9
Morten Gross <mg@vuffeli.dk>
2021-12-07 21:47:34 +0100

    Cleaned the markup a bit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,13 +6,10 @@ import Header from "../Globals/Header";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-       <Header/>
-        <div className="bg-background">
-            <div className="max-w-screen-2xl mx-auto grid grid-cols-8 gap-4">
-                <Component {...pageProps} />
-            </div>
-        </div>
-
+      <Header/>
+      <main className="2xl:container mx-auto bg-background">
+        <Component {...pageProps} />
+      </main>
       <Footer/>
     </>
 )
