91ea44801fa64e2db2e40ef13bfa20572f8e99c3
Morten Gross <mg@vuffeli.dk>
2021-12-10 15:47:37 +0100

    implemented ImageSection component

--- src/pages/learn/monstre.js
+++ src/pages/learn/monstre.js
@@ -1,4 +1,5 @@
 import { request } from "../../../lib/datocms";
+import ImageSection from "../../components/ImageSection";
 
 export default function LearnToKnit(data) {
     const blocksArray = data.data.content.blocks;
@@ -12,26 +13,9 @@ export default function LearnToKnit(data) {
                 </article>
             </section>
 
-            {blocksArray.map(block => {return <BlockSection key={block.id} index={blocksArray.findIndex(item => item.id === block.id)} {...block}></BlockSection>})}
+            {blocksArray.map(block => {return <ImageSection key={block.id} index={blocksArray.findIndex(item => item.id === block.id)} {...block}></ImageSection>})}
         </>
     )
-}
-
-function BlockSection(props) {
-    return (
-        <>
-        <section className="w-full mb-4">
-            <article className="grid grid-cols-6 gap-4 flex flex-wrap">
-                <img loading="lazy" src={props.mediaasset.url} alt={props.mediaasset.alt} className={"col-span-6 lg:col-span-3 " + ((props.index % 2 > 0) ? "lg:order-2" : "lg:order-1")}></img>
-                <div className={"col-span-6 lg:col-span-3 p-16 self-center " + ((props.index % 2 > 0) ? "lg:order-1" : "lg:order-2")}>
-                    <span className="font-sans text-sm text-black-60">{props.assetnumber}.</span>
-                    <h2 className="font-serif text-xl lg:text-3xl mb-4">{props.header}</h2>
-                    <p className="text-black-60">{props.body}</p>
-                </div>
-            </article>
-        </section>
-        </>
-    );
 }
 
 const learnData = `query learnPage {
