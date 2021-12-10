a1490142ea9f0c6b547e5ff0a00cb8406a6d3cf6
Morten Gross <mg@vuffeli.dk>
2021-12-10 15:31:26 +0100

    Redid order classes

--- src/pages/learn/forstaaopskrifter.js
+++ src/pages/learn/forstaaopskrifter.js
@@ -18,13 +18,12 @@ export default function LearnToKnit(data) {
 }
 
 function BlockSection(props) {
-    console.log(props.index % 2);
     return (
         <>
         <section className="w-full mb-4">
             <article className="grid grid-cols-6 gap-4 flex flex-wrap">
-                <img loading="lazy" src={props.mediaasset.url} alt={props.mediaasset.alt} className={((props.index % 2 > 0) ? "lg:order-2 " : "") + "col-span-6 lg:col-span-3"}></img>
-                <div className={"col-span-6 lg:col-span-3 p-16 self-center "}>
+                <img loading="lazy" src={props.mediaasset.url} alt={props.mediaasset.alt} className={"col-span-6 lg:col-span-3 " + ((props.index % 2 > 0) ? "lg:order-2" : "lg:order-1")}></img>
+                <div className={"col-span-6 lg:col-span-3 p-16 self-center " + ((props.index % 2 > 0) ? "lg:order-1" : "lg:order-2")}>
                     <span className="font-sans text-sm text-black-60">{props.assetnumber}.</span>
                     <h2 className="font-serif text-xl lg:text-3xl mb-4">{props.header}</h2>
                     <p className="text-black-60">{props.body}</p>
